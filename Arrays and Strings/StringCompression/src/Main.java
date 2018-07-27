d4c8191d53cb99086c92a073b7116bded397071d
Han (Mia) Ngo <ngo.gh98@gmail.com>
2018-07-27 13:38:51 -0600

    Problem 1.7 StringCompression

--- Arrays and Strings/StringCompression/src/Main.java
+++ Arrays and Strings/StringCompression/src/Main.java
@@ -1,10 +1,11 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 /*
  * String Compression: Implement a method to perform basic string compression
  * using the counts of repeated characters. For example, the string aabcccccaaa
- * would become a2blc5a3. If the "compressed" string would not become smaller
+ * would become a2b1c5a3. If the "compressed" string would not become smaller
  * than the original string, your method should return the original string.
  * You can assume the string has only uppercase and lowercase letters (a - z).
  */
@@ -21,13 +22,50 @@ public class Main {
         scan.close();
 
         for (String s : strings) {
-            String[] words = s.split("\\s+");
-            System.out.println(isOneAway_cases(words[0], words[1]));
+            System.out.println(compress(s));
         }
     }
 
     /* Time Complexity:  O() */
-    private static boolean isOneAway_cases(String original, String edit) {
+    private static StringBuilder compress(String s) {
+        if (isUnique(s)) {
+            return new StringBuilder(s);
+        }
 
+        StringBuilder result = new StringBuilder();
+        int count = 1;
+        for (int i = 1; i < s.length(); i++) {
+            char prev = s.charAt(i-1);
+            if (s.charAt(i) == prev) {
+                count++;
+                if (i == s.length() - 1){
+                    result.append(prev);
+                    result.append(count);
+                }
+            } else {
+                result.append(prev);
+                result.append(count);
+                count = 1;
+                if (i == s.length() - 1){
+                    result.append(s.charAt(i));
+                    result.append(count);
+                }
+            }
+        }
+        return result;
+    }
+
+    /* Time Complexity: O(n) */
+    private static boolean isUnique(String s) {
+        int MAX_LENGTH = 256;
+        int[] ascii = new int[MAX_LENGTH];
+        for (int i = 0; i < s.length(); i++) {
+            int value = (int) s.charAt(i);
+            ascii[value]++;
+            if (ascii[s.charAt(i)] > 1) {
+                return false;
+            }
+        }
+        return true;
     }
 }
