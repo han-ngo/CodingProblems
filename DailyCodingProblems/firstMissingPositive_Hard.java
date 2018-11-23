34f33e4d3c0f2bd0e3fd9ee93c16bf2afb4915d6
Han (Mia) Ngo <ngo.gh98@gmail.com>
2018-11-22 22:58:52 -0700

    improve firsMissingPosition from O(nlogn) to O(n)

--- DailyCodingProblems/firstMissingPositive_Hard.java
+++ DailyCodingProblems/firstMissingPositive_Hard.java
@@ -1,5 +1,37 @@
 class Solution {
+    /* Time Complexity: O(n) */
     public int firstMissingPositive(int[] nums) {
+        if (nums.length == 0) {
+            return 1;
+        }
+
+        int i = 0;
+        while (i < nums.length) {
+            if (nums[i] <= 0 || nums[i] > nums.length || nums[nums[i] - 1] == nums[i]) {
+                /* Skip negative, out of length confinement, or already in correct place */
+                i++;
+            } else if (nums[nums[i] - 1] != nums[i]) {
+                swap(nums, i, nums[i] - 1); // Swap to the correct place
+            } else {
+                i++;
+            }
+        }
+
+        i = 0;
+        while (i < nums.length && nums[i] == i + 1) {
+            i++;
+        }
+        return i + 1;
+    }
+
+    private void swap(int[] arr, int i1, int i2) {
+        int temp = arr[i1];
+        arr[i1] = arr[i2];
+        arr[i2] = temp;
+    }
+
+    /* Time Complexity: O(nlogn) */
+    public int firstMissingPositive_Sort(int[] nums) {
         if (nums.length == 0) {
             return 1;
         }
